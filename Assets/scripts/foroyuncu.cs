4f9c6bb2b142b62150920a60a91d5e43d740acee
bcgkky <bcgkky1@gmail.com>
2023-12-07 11:52:14 +0300

    fnsh
    ...

---
+++ Assets/scripts/foroyuncu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foroyuncu : MonoBehaviour
+{
+    Rigidbody fz;
+    public float hiz;
+    void Start()
+    {
+        fz = GetComponent<Rigidbody>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        float yatay = Input.GetAxis("Horizontal");
+        float dikey = Input.GetAxis("Vertical");
+
+        Vector3 vk = new Vector3(yatay, 0, dikey);
+
+        fz.AddForce(vk*hiz);
+    }
+}
---
+++ Assets/scripts/kameramanbenidecek.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kameramanbenidecek : MonoBehaviour
+{
+    Vector3 fark;
+    public Transform top;
+
+    [Range(0.01f, 1.0f)]
+    public float smoothfactor = 0.5f;
+    public bool lookatplayer = false;
+    public bool rotatearoundplayer = true;
+    public float rotationsspeed = 5.0f;
+    void Start()
+    {
+        fark = transform.position - top.position;
+    }
+
+    void LateUpdate()
+    {
+
+        if(rotatearoundplayer)
+        {
+            Quaternion camturnangle = 
+                Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationsspeed, Vector3.down);
+
+            fark = camturnangle * fark;
+        }
+
+        Vector3 newPos = top.position + fark;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothfactor);  //transform.position = fark + top.transform.position;
+
+
+
+        if (lookatplayer || rotatearoundplayer)
+        {
+            transform.LookAt(top);
+        }
+    }
+}
